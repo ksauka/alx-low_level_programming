a0ae16fd07301faea09a7f0ff58f4d3aed3eeb3d
ksauka <kudzysauka@gmail.com>
2022-09-12 09:31:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Alphabet
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+
+return (0);
+}
