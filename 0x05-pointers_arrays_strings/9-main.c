1754097d8527019bd962c702cdfc59b2276b88bd
ksauka <kudzysauka@gmail.com>
2022-09-20 11:41:37 -0700

    strcoy

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	char s1[98];
-	char *p;
+	char *ptr;
 
 
 	p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
 	printf("%s", s1);
-	printf("%s", p);
+	printf("%s", ptr);
 	return (0);
 
 }
