b3669844530747746189610e56e142aa548ebc43
ksauka <kudzysauka@gmail.com>
2022-09-22 04:17:15 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 
 
 /**
- * reverse_array - a function that reverses the content 
+ * reverse_array - a function that reverses the content
  * of an array of integers.
  * @a: an array of integers
  * @n: the number of elements to swap
