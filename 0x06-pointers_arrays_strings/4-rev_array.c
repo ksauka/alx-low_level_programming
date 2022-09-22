eaf457a9708d92eb8bc5c872e054ff8c0466004e
ksauka <kudzysauka@gmail.com>
2022-09-22 04:10:53 -0700

    ./4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_array - a function that reverses the content of an array of integers.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: Always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int aux;
+
+
+	while (i < n--)
+	{
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
+}
+
+
