e995a7cb92f30e4dfc49c5e5f90e368e3b52b676
ksauka <kudzysauka@gmail.com>
2022-10-04 10:18:14 -0700

    grid

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - A function that concatenates two strings
+ * @s1: An input pointer of the first string
+ * @s2: An input pointer of the second string
+ * eturn: Apointer to concatened strings or NULL if it str is NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+
+
+
+
+}
